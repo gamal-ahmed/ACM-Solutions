34666b297e1149473456e4f9860f2c34b2bc4334
Ahmed Gamal <ahmed.gamal@scope-eg.com>
2016-03-27 19:17:44 +0200

    Added files via upload

---
+++ A. Magnets.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+using namespace std;
+
+int mainaw()
+{
+        int  magents [100000];
+        int groups=0;
+        int group01=0;
+        int cuurent,last;
+        int n;
+        cin>>n;
+        bool newGroup=true;
+        for(int i=0;i<n;i++)
+        {
+            cin>>magents[i];
+            cuurent=magents[i];
+            if(i!=0)
+            last=magents[i-1];
+            if(last!=cuurent)
+            groups++;
+        }
+
+        cout<<groups;
+
+return 0;
+
+}
+
